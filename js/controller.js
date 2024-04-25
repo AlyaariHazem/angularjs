a3eee29bbb7a650f4ba12d49a22d39f953393fa0
haz10em <haz10em@gmail.com>
2024-04-26 02:06:52 +0300

    learing AngularJs

--- js/controller.js
+++ js/controller.js
@@ -1,36 +1,38 @@
-var myapp=angular.module('myapp',[]);
-myapp.controller('Mycontroller',function fanController($scope){
+var myapp = angular.module('myapp', []);
 
-    $scope.students=[
-        {  
-            'name':'Hazem',
-            'gender':'Male',
-            'physics':'88',
-            'maths':'87',
-            'english':'78'
-         },
-         {  
-            'name':'Fahem',
-            'gender':'Male',
-            'physics':'96',
-            'maths':'100',
-            'english':'95'
-         },
-         {  
-            'name':'Mohammed',
-            'gender':'Male',
-            'physics':'89',
-            'maths':'90',
-            'english':'70'
-         },
-         {  
-            'name':'Ali',
-            'gender':'Male',
-            'physics':'30',
-            'maths':'25',
-            'english':'40'
-         }     
-        ];
-    
-
+myapp.controller('Mycontroller', function fanController($scope, $http) {
+    $http.get('../student-data.json').then(function(response) {
+        $scope.students = response.data;
+    });
 });
+    // $scope.disstudents=[
+    //     {  
+    //         'name':'Hazem',
+    //         'gender':'Male',
+    //         'physics':'88',
+    //         'maths':'87',
+    //         'english':'78'
+    //      },
+    //      {  
+    //         'name':'Fahem',
+    //         'gender':'Male',
+    //         'physics':'96',
+    //         'maths':'100',
+    //         'english':'95'
+    //      },
+    //      {  
+    //         'name':'Mohammed',
+    //         'gender':'Male',
+    //         'physics':'89',
+    //         'maths':'90',
+    //         'english':'70'
+    //      },
+    //      {  
+    //         'name':'Ali',
+    //         'gender':'Male',
+    //         'physics':'30',
+    //         'maths':'25',
+    //         'english':'40'
+    //      }     
+    //     ];
+   
